513d3f72ee63b1134ae2bd099a6ed53a001c3d20
action <action@github.com>
2025-08-05 05:27:07 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,6 +1,14 @@
-//Tue Aug 05 2025 05:26:03 GMT+0000 (Coordinated Universal Time)
+//Tue Aug 05 2025 05:27:07 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function getSecretKey() {
-  return "*****辞晨#####";
+function koso() {
+  const {
+    java: _0x32c3f8,
+    source: _0x33cb69
+  } = this;
+  let _0x5429f5 = _0x32c3f8.get("http://gednvy.yesui.me/", {
+    "Content-Type": "application/json",
+    "Accept": String("application/json, text/plain, */*")
+  }).body();
+  return _0x5429f5;
 }
